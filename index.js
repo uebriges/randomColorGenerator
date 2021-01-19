723f336974be85c9b5ab81e608bc25eb268d92d9
Uebriges <uebriges@gmx.at>
2021-01-19 17:45:55 +0100

    Added the 'ask' fuctionality'

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 const randomColor = require('randomcolor');
 const chalk = require('chalk');
+const readline = require('readline-sync');
+
+const regex = new RegExp('^\\d{1,2}x\\d{1,2}$');
 
 if (!process.argv[2]) {
   drawBox();
-} else if (process.argv[2] && process.argv[3]) {
-  drawBox();
+} else if (process.argv[2]) {
+  if (process.argv[2] === 'ask') {
+    const color = readline.question('Color: ');
+    const lum = readline.question('Luminosity: ');
+    let colorhex = setColor(color, lum);
+    drawBox(colorhex);
+  } else if (regex.test(process.argv[2])) {
+    if (
+      process.argv[2].split('x')[0] < 9 &&
+      process.argv[2].split('x')[0] < 31
+    ) {
+      console.log('Minium: 9x31');
+    } else {
+      drawBox(
+        undefined,
+        process.argv[2].split('x')[0],
+        process.argv[2].split('x')[1],
+      );
+    }
+  }
 }
 
-function drawBox(lines, characters) {
+function drawBox(color, lines, characters) {
+  // If no lines or characters given default -> 9x31
   !lines ? (lines = 9) : (lines = lines);
   !characters ? (characters = 31) : (characters = characters);
 
-  let color = setColor(process.argv[2], process.argv[3]);
-  let boxString = '';
+  // If no color given ->
+  if (!color) {
+    color = setColor();
+  } else {
+    color = color;
+  }
 
-  console.log(lines);
-  console.log(characters);
+  let boxString = '';
 
   for (let i = 1; i <= lines; i++) {
     if (i <= 3) {
@@ -37,6 +62,5 @@ function drawBox(lines, characters) {
 function setColor(hue, luminosity) {
   !hue ? (hue = 'random') : (hue = hue);
   !luminosity ? (luminosity = 31) : (luminosity = luminosity);
-
   return randomColor({ luminosity: luminosity, hue: hue });
 }
